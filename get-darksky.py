2511acc5d2a3b3322de58daa2b761e1fd6c18c43
psmith <psmith@nuvectramed.com>
2019-03-11 07:12:03 -0600

    Add summary to forecasts, remove commented out code, add gitignore. Still ugly.

--- get-darksky.py
+++ get-darksky.py
@@ -15,22 +15,17 @@ MB_TO_INHG = 0.029530
 
 client = DarkSkyClient(api_key, (lat, lon), units="us")
 
-#printf "  Now: %-#{width}s %.0f°%s, Wind %s, Humidity %s, Barometer %s %s\n", 
 print("Summary: " + client.raw_data['daily']['summary'])
-#print("Now: " + client.currently.summary + " " + \
-#      str(int(client.currently.temperature+0.5)) + "°F, " + \
-#      "Wind " + str(client.currently.windSpeed) + "mph bearing " + \
-#      str(client.currently.windBearing) + \
-#      ", Humidity " + str(client.currently.humidity*100) + "% " + \
-#      ", Barometer " + str(client.currently.pressure*MB_TO_INHG))
+
 print("Now: %s %d°F, Wind %smph from %s, Humidity %d%%, Barometer %.2f" %\
       (client.currently.summary, int(client.currently.temperature+0.5),
       str(client.currently.windSpeed),str(client.currently.windBearing),
       client.currently.humidity*100,client.currently.pressure*MB_TO_INHG))
 
 for i in range(3):
-   print("%s: %d°F - %d°F" % \
+   print("   %s: %s, %d°F - %d°F" % \
         (date.fromtimestamp(client.daily.data[i]['time']).strftime("%a"),\
+         client.daily.data[i]['summary'], \
          client.daily.data[i]['temperatureHigh'], \
          client.daily.data[i]['temperatureLow']))
          
