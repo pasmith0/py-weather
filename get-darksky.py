77f6601514e538ca42e38893e0b13e2485e9dd46
psmith <psmith@nuvectramed.com>
2019-03-12 12:23:05 -0600

    Indent

--- get-darksky.py
+++ get-darksky.py
@@ -15,15 +15,15 @@ MB_TO_INHG = 0.029530
 
 client = DarkSkyClient(api_key, (lat, lon), units="us")
 
-print("Summary: " + client.raw_data['daily']['summary'])
+print("   Summary: " + client.raw_data['daily']['summary'])
 
-print("Now: %s %d°F, Wind %smph from %s, Humidity %d%%, Barometer %.2f" %\
+print("   Now: %s %d°F, Wind %smph from %s, Humidity %d%%, Barometer %.2f" %\
       (client.currently.summary, int(client.currently.temperature+0.5),
       str(client.currently.windSpeed),str(client.currently.windBearing),
       client.currently.humidity*100,client.currently.pressure*MB_TO_INHG))
 
 for i in range(3):
-   print("   %s: %s, %d°F - %d°F" % \
+   print("   %s: %s.. High %d°F, Low %d°F" % \
         (date.fromtimestamp(client.daily.data[i]['time']).strftime("%a"),\
          client.daily.data[i]['summary'], \
          client.daily.data[i]['temperatureHigh'], \
